1b427a6a6eaf64bf2f257c199ce0b2d2a7c19fd9
Utsav Raj <rajutsav1234@gmail.com>
2023-08-28 15:42:32 +0530

    payee done

---
+++ src/main/java/com/wellsfargo/training/obs/model/Payee.java
@@ -0,0 +1,66 @@
+package com.wellsfargo.training.obs.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Payee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long payeeID;
+	private String payeeNickname;
+	private String payeeAccountNo;
+	private String payerAccountNo;
+	
+	public Payee() {
+	}
+
+	public Payee(Long payeeID, String payeeNickname, String payeeAccountNo, String payerAccountNo) {
+		this.payeeID = payeeID;
+		this.payeeNickname = payeeNickname;
+		this.payeeAccountNo = payeeAccountNo;
+		this.payerAccountNo = payerAccountNo;
+	}
+
+	public Long getPayeeID() {
+		return payeeID;
+	}
+
+	public void setPayeeID(Long payeeID) {
+		this.payeeID = payeeID;
+	}
+
+	public String getPayeeNickname() {
+		return payeeNickname;
+	}
+
+	public void setPayeeNickname(String payeeNickname) {
+		this.payeeNickname = payeeNickname;
+	}
+
+	public String getPayeeAccountNo() {
+		return payeeAccountNo;
+	}
+
+	public void setPayeeAccountNo(String payeeAccountNo) {
+		this.payeeAccountNo = payeeAccountNo;
+	}
+
+	public String getPayerAccountNo() {
+		return payerAccountNo;
+	}
+
+	public void setPayerAccountNo(String payerAccountNo) {
+		this.payerAccountNo = payerAccountNo;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/wellsfargo/training/obs/repository/PayeeRepository.java
@@ -0,0 +1,11 @@
+package com.wellsfargo.training.obs.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wellsfargo.training.obs.model.Payee;
+
+public interface PayeeRepository extends JpaRepository<Payee,Long> {
+//        public Optional<Payee> findByaccountnumber(Long accountnumber);
+}
